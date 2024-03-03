34f5644b53632ef2eaa206a3b0c2fd1ce36ebb04
henriqueEz4568 <rick.scioli@gmail.com>
2024-03-03 15:31:35 -0300

    :)

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const rotasProjeto=require('./routers/router.js')
 app.engine('ejs', require('ejs').__express);
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
-
+const port=5000
 const rotasLogin = require('./routers/routerlogin.js')
 const rotasCadastro = require('./routers/routerCadastro.js')
 
-app.listen(5000, ()=>console.log('servidor rodando na porta 5000'))
+app.listen(port, ()=>console.log(`servidor rodando na port: ${port}`))
  //app.get('/', (req,res)=>res.send('hello world'))
 app.use('/',rotasProjeto)
 app.use('/home/',rotasLogin)
